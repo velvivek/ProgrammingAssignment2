4454545c49f5ee410118578da4eb6953fe26a161
velvivek <nagarajavel@yahoo.com>
2017-11-19 18:56:21 -0500

    completed matrix inversion problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates list for getting the values for inversion of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inversion <- NULL
+  set <- function(y) {
+    x <<- y
+    inversion <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inversion <<- inverse
+  getinverse <- function() inversion
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function generates the inverse of the matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversion <- x$getinverse()
+  if(!is.null(inversion)) {
+    return(inversion)
+  }
+  data <- x$get()
+  inversion <- solve(data)
+  x$setinverse(inversion)
+  inversion
 }
+
